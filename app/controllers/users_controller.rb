d39b3f866f3da27216a2ee814c6dcd2d85c65277
ryota <siroisagani@gmail.com>
2018-02-04 18:09:01 +0900

    ユーザーコントローラーのpasswordをpassword_digestに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmaito)
+    params.require(:user).permit(:email, :password_digest, :password_confirmaito)
   end
 end
