c5814296fed0ae3a02aa2f53c9873aa1692cc8e6
ryota <siroisagani@gmail.com>
2018-02-04 18:12:49 +0900

    has_secure_passwordの使用と、passwordのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :blogs
   validates :email, uniqueness: true, presence: true, length: { maximum: 255 },
                   format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
