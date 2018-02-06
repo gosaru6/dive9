4e49ccc3f8beb94f9c20ee3e2e797189cbf28597
ryota <siroisagani@gmail.com>
2018-02-06 15:45:38 +0900

    favoriteコントローラーにcreate,destroyアクションを追加

--- app/controllers/favorite_controller.rb
+++ app/controllers/favorite_controller.rb
@@ -1,2 +1,20 @@
 class FavoriteController < ApplicationController
+  respond_to :js
+
+  def create
+    if logged_in?
+      @blog = Blog.find(params[:favorite][:blog_id])
+      current_user.favorite!(@blog)
+      respond_with @blog
+    else
+      respond_with @blog
+    end
+  end
+
+  def destroy
+    @blog = Favorite.find(params[:id]).blog
+    current_user.unfavorite!(@blog)
+    respond_with @blog
+  end
+
 end
