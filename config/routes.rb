5a387316a4040de0b3ed1be09b08e3701628c2cb
ryota <siroisagani@gmail.com>
2018-02-06 13:13:22 +0900

    Close #11

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'blogs#index'
   resources :blogs
-  resources :users
+  resources :users, only: [:new, :create, :show, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
 end
