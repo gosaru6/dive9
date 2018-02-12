5fd94253e179166aee9c4c9f11414425c9498188
ryota <siroisagani@gmail.com>
2018-02-12 15:44:23 +0900

    Close #23

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true, presence: true, length: { maximum: 255 },
                   format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  
+  has_secure_password
+
 end
