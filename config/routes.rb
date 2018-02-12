a6a886386c3e8eeda81561babeab42a32c03bd4c
ryota <siroisagani@gmail.com>
2018-02-12 11:54:53 +0900

    Close #21

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'blogs#index'
   resources :blogs
-  resources :users, only: [:new, :create, :show, :destroy]
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :favorites, only: [:create, :destroy]
+  resources :users     , only: [:new, :create, :show, :destroy]
+  resources :sessions  , only: [:new, :create, :destroy]
+  resources :favorites , only: [:create, :destroy]
 end
